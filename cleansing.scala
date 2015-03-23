8c10da84271af5d05a0ba31406ebc58cb7919651
Hua-Qiang Wang <hua-qiang.wang@hp.com>
2015-03-23 18:16:21 +0800

    2nd Chapter finished

--- cleansing.scala
+++ cleansing.scala
@@ -123,5 +123,75 @@ object NAStatCounter extends Serializable {
   def apply(x: Double) = new NAStatCounter().add(x)
 }
 
+val nastats = NAStatCounter.apply(17.29)
+val nastats = NAStatConter(17.29)
+
+val nas1 = NAStatCounter(10.0)
+nas1.add(2.1)
+val nas2 = NAStatCounter(Double.NaN)
+nas1.merge(nas2)
+
+
+val arr = Array(1.0, Double.NaN, 17.29)
+val nas = arr.map(d => NAStatCounter(d))
+
+// apply this operation on RDD
+val nasRDD = parsed.map(md => {
+    md.scores.map(d => NAStatCounter(d))
+})
+
+
+val nas1 = Array(1.0, Double.NaN).map(d => NAStatCounter(d))
+val nas2 = Array(Double.NaN, 2.0).map(d => NAStatCounter(d))
+val merged = nas1.zip(nas2).map(p => p._1.merge(p._2))
+
+val merged = nas1.zip(nas2).map{ case (a, b) => a.merge(b)}
+
+// with a reduce method
+val nas = List(nas1, nas2)
+val merged = nas.reduce((n1, n2) => {
+    n1.zip(n2).map{case (a, b) => a.merge(b)}
+})
+
+// reduce on RDD
+val reduced = nasRDD.reduce((n1, n2) => {
+    n1.zip(n2).map{ case (a, b) => a.merge(b)}
+})
+
+
+// StatsWithMissing.scala
+import org.apache.spark.rdd.RDD
+def statsWithMissing(rdd: RDD[Array[Double]]): Array[NAStatCounter] = {
+val nastats = rdd.mapPartitions( (iter: Iterator[Array[Double]]) => {
+val nas: Array[NAStatCounter] = iter.next().map(d => NAStatCounter(d))
+iter.foreach(arr => {
+nas.zip(arr).foreach{ case (n, d) => n.add(d) }
+})
+Iterator(nas)
+})
+nastats.reduce((n1, n2) => {
+n1.zip(n2).map {case (a, b) => a.merge(b) }
+})
+}
+
+//Simple Variable Selection and Scoring
+val statsm = statsWithMissing(parsed.filter(_.matched).map(_.scores))
+val statsn = statsWithMissing(parsed.filter(!_.matched).map(_.scores))
+
+statsm.zip(statsn).map { case(m,n) =>
+  (m.missing + n.missing, m.stats.mean - n.stats.mean)
+}.foreach(println)
+
+
+// use a simple scoring model that ranks the similarity of pairs of records
+// based on the sums of the values of the obviously good features
+
+def naz(d: Double) = if (Double.NaN.equals(d)) 0.0 else d
+case class Scored(md: MatchData, score: Double)
+val  ct = parsed.map(md => {
+val  score = Array(2, 5, 6, 7, 8).map(i => naz(md.scores(i))).sum
+Scored(md, score)
+})
+
 
 
