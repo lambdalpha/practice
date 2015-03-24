43936c7424f360cea5fc373734a171355760f48d
Hua-Qiang Wang <hua-qiang.wang@hp.com>
2015-03-24 14:00:36 +0800

    Added chapter 3 code

---
+++ recommend.scala
@@ -0,0 +1,4 @@
+// Chapter 3 Recommendig music and the audioscrobbler data set
+// Using ASL algorithm
+// load the data
+val rawUserArtistData = sc.textFile("/opt/mount/spark/profiledata_06-May-2005/user_artist_data.txt")
