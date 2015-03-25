40c549838d2dfcfc627ef0208432b26c9098eba6
Hua-Qiang Wang <hua-qiang.wang@hp.com>
2015-03-25 18:25:02 +0800

    chapter 3 kick off

--- recommend.scala
+++ recommend.scala
@@ -2,3 +2,13 @@
 // Using ASL algorithm
 // load the data
 val rawUserArtistData = sc.textFile("/opt/mount/spark/profiledata_06-May-2005/user_artist_data.txt")
+
+val rawArtistData = sc.textFile("/opt/mount/spark/profiledata_06-May-2005/artist_data.txt")
+
+// this may cause some error, for same 
+val artistByID = rawArtistData.map { line => 
+    val (id, name) = line.span(_ != '\t')
+    (id.toInt, name.trim)
+}
+
+
